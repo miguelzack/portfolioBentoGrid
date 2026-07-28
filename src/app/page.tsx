df4e91febc3a6f9d88e37edef4b52c6d995cc8e8
miguelzackk <mzsilvaw@gmail.com>
2026-07-28 15:58:21 -0300

    feat: created cardGrid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
+import SvgComponent from "@/assets/icons/rocketIcon";
+import {Card} from "@/components/cardGrid/cardGrid";
+import RocketIcon from "@/assets/icons/rocketIcon";
+import StacksIcon from "@/assets/icons/stacksIcon";
+import ProjectsIcon from "@/assets/icons/projectsIcon";
+
 export default function Home() {
     return (
-        <main className="mx-auto grid min-h-screen max-w-[1440px] grid-cols-8 grid-rows-4 gap-[42px] p-[42px]">
+        <main className="mx-auto grid min-h-screen max-w-360 grid-cols-8 grid-rows-4 gap-10.5 p-10.5">
 
-            {/*TRANSFORMAR ESSAS DIV EM COMPONENT (Follow, Certifications, My Sctacks, Projects*/}
+            <Card icon={<RocketIcon/>} subTitle={"Follow Me"} title={"Online Presence"}
+                  className={"col-start-1 col-span-2 row-start-1 row-span-1"}/>
 
-            <div className="col-start-1 col-span-2 row-start-1 row-span-1 rounded-3xl border-2 p-6">
-                Follow Me
-            </div>
-
-            <div className="col-start-1 col-span-2 row-start-2 row-span-3   rounded-3xl border-2  p-6">
-                Certifications
-            </div>
+            <Card icon={<RocketIcon/>} subTitle={"My Certifications"} title={"Certifications"}
+                  className={"col-start-1 col-span-2 row-start-2 row-span-3"}/>
 
             <div className="col-start-3 col-span-4 row-start-1 row-span-2 rounded-3xl border-2 p-6">
                 About me
             </div>
 
-            <div className="col-start-3 col-span-2 row-start-3 row-span-2 rounded-3xl border-2 p-6">
-                Tech Arsenal
-            </div>
+            <Card icon={<StacksIcon/>} subTitle={"My Stacks"} title={"Tech Arsenal"}
+                  className={"col-start-3 col-span-2 row-start-3 row-span-2"}/>
 
-            <div className="col-start-5 col-span-4 row-start-3 row-span-2 rounded-3xl border-2 p-6">
-                Repositories
-            </div>
+
+            <Card icon={<ProjectsIcon/>} subTitle={"My Projects"} title={"Repositories"}
+                  className={"col-start-5 col-span-4 row-start-3 row-span-2"}/>
 
             <div className="col-start-7 col-span-2 row-start-1 row-span-2 rounded-3xl border-2 p-6">
                 Let's Work Together
--- src/components/cardGrid/cardGrid.tsx
+++ src/components/cardGrid/cardGrid.tsx
@@ -1,4 +1,41 @@
-interface CardGridProps {
-    title: string;
+import {ReactNode} from "react";
+
+interface CardProps {
+    icon: ReactNode;
     subTitle: string;
+    title: string;
+    className?: string;
+    buttons: ReactNode;
+}
+
+export function Card({
+                         icon,
+                         subTitle,
+                         title,
+                         className = "",
+                         buttons
+                     }: CardProps) {
+    return (
+        <div className={`rounded-3xl border-2 p-6 gap-2.5 flex flex-col self-stretch${className}`}>
+
+            <div className="flex flex-col self-stretch items-center">
+                <div className="flex px-2.5 py-1.5 items-center gap-2 justify-center">
+                    {icon}
+                    <h3 className="text-gray-font-primary text-sm font-medium">
+                        {subTitle}
+                    </h3>
+                </div>
+
+                <h2 className="text-white text-base font-semibold leading-6">
+                    {title}
+                </h2>
+            </div>
+
+
+            <div className="{className}">
+                {buttons}
+            </div>
+        </div>
+
+    );
 }
